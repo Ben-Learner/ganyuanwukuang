beda1debe7ad57d93c7626c9ea049f2b9dfe6c18
qiben-jy <qb19@mails.tsinghua.edu.cn>
2022-04-09 22:39:03 +0800

    add DADE Prohit

--- Earnings calculation.py
+++ Earnings calculation.py
@@ -6,14 +6,14 @@
 """
 # 定义年产量(t)，价格(万元/t)
 Basic_Production = 300000
-Dynamic_Production = 330000
+Dynamic_Production = 300000
 Production_Factor = Dynamic_Production / Basic_Production #大于1
 Price_Factor = 1 #大于1
-wu = [2525 * Production_Factor, 12 * Price_Factor]
-mu = [32 * Production_Factor, 8 * Price_Factor]
-tong = [338 * Production_Factor, 0.7 * Price_Factor]
+wu = [4253.7 * Production_Factor, 15.3* Price_Factor]
+mu = [23.1 * Production_Factor, 6 * Price_Factor]
+tong = [669.9* Production_Factor, 0.7 * Price_Factor]
 bi = [43 * Production_Factor, 2 * Price_Factor]
-xi = [81 * Production_Factor, 5 * Price_Factor]
+xi = [300.3 * Production_Factor, 5 * Price_Factor]
 All_Metal = [wu, mu, tong, bi, xi]
 Sales_revenues = 0
 # 年销售额（万元）
@@ -29,13 +29,25 @@ VAT_rate = 0.1185
 Sales_Tax_rate = 0.06463
 #所得税
 Income_Tax_Rate = 0.25
+#税前净利润
+Profit_Before_Tax = (Sales_revenues *(1 - VAT_rate - Sales_Tax_rate) - Total_Cost_Year)
+print('税前净利润：',Profit_Before_Tax)
 #税后净利润
 Profit_After_Tax = (Sales_revenues *(1 - VAT_rate - Sales_Tax_rate) - Total_Cost_Year) * (1 - Income_Tax_Rate)
 print('税后净利润：',Profit_After_Tax)
-#平均未分配利润
-Average_Undistributed_Profit = Profit_After_Tax * (1 - 0.1)
+#大德同策投资有限公司权益
+Before_DADE = Profit_Before_Tax * 2/3 * 0.6 #税前收益
+After_DADE = Profit_After_Tax * 2/3 * 0.6 #税后收益
+print('税前大德同策投资有限公司权益：',Before_DADE)
+print('税后大德同策投资有限公司权益：',After_DADE)
+# #平均未分配利润
+# Average_Undistributed_Profit = Profit_After_Tax * (1 - 0.1)
+#项目投资总额
+Project_Investment = 37297.21
+#项目投资回收期（税前）
+Before_Payback_Time = Project_Investment/Before_DADE
+print('项目投资回收期（税前）：',Before_Payback_Time)
 #项目投资回收期（税后）
-Project_Investment = 42619.11 #项目投资总额
-Payback_Time = Project_Investment/Profit_After_Tax
-print('项目投资回收期：',Payback_Time)
+After_Payback_Time = Project_Investment/After_DADE
+print('项目投资回收期（税后）：',After_Payback_Time)
 
